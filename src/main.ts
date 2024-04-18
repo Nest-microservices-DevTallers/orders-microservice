73f3bc66880e624a7b7f6ec701a65e982441daa4
BetoNajera <ing.betonajera@gmail.com>
2024-04-18 12:43:53 -0600

    :sparkles: Feat: Transform app to microservice
    Transform app to microservice

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,24 @@
+import { Transport } from '@nestjs/microservices';
 import { NestFactory } from '@nestjs/core';
+import { Logger } from '@nestjs/common';
+
 import { AppModule } from './app.module';
+import { envs } from './config';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  const logger = new Logger('OrdersBoostrap');
+
+  const app = await NestFactory.createMicroservice<MicroserviceOptions>(
+    AppModule,
+    {
+      transport: Transport.TCP,
+      options: {
+        port: envs.port,
+      },
+    },
+  );
+
+  await app.listen();
+  logger.log(`🚀 Orders microdervice running on port ${envs.port}`);
 }
 bootstrap();
